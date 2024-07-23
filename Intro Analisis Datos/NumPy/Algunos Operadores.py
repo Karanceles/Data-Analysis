c053a3a5f89e2d8905d724a3f320424f58ef2963
Leonora Villalobos Burgos <lucy.rjsoo@gmail.com>
2024-07-23 01:12:18 -0400

    init

---
+++ Intro Analisis Datos/NumPy/Algunos Operadores.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Vectores
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+
+# Producto escalar
+producto_escalar = np.dot(v1, v2)
+print("Producto escalar:", producto_escalar)
+
+# Producto punto
+producto_punto = np.dot(v1, v2)
+print("Producto punto:", producto_punto)
+
